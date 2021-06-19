88363d82bb33122ec7144847af72b4fecc720744
Bruce Trindade <brucetrindade2000@gmail.com>
2021-06-19 20:53:04 -0300

    Coala Versão 1.02.04

--- app/src/main/java/com/example/coalamobile/HomeActivity.kt
+++ app/src/main/java/com/example/coalamobile/HomeActivity.kt
@@ -6,30 +6,17 @@ import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.drawerlayout.widget.DrawerLayout
-import androidx.fragment.app.Fragment
-import com.example.coalamobile.ui.login.LoginFragment
 import com.google.android.material.navigation.NavigationView
-
 
 class HomeActivity : AppCompatActivity() {
 
     lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         setSupportActionBar(findViewById(R.id.toolbar))
-        //Meu e-mail: brucetrindade2000@gmail.com
-        //Setando o fragment do login firebase
-        if (savedInstanceState == null){
-            supportFragmentManager
-                    .beginTransaction()
-                    .add(R.id.drawerLayout, LoginFragment())
-                    .commit()
-        }
 
-        //setando a gaveta de menu
         val drawerLayout : DrawerLayout = findViewById(R.id.drawerLayout)
         val navView : NavigationView = findViewById(R.id.nav_view)
 
