2a3fe0072035778b300623c98e7cd6d99723f3e8
Bruce Trindade <brucetrindade2000@gmail.com>
2021-06-20 20:57:58 -0300

    Coala Versão 1.02.08

---
+++ app/src/main/java/com/example/coalamobile/Login.kt
@@ -0,0 +1,8 @@
+package com.example.coalamobile
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Login {
+
+}
---
+++ app/src/main/java/com/example/coalamobile/ui/login/LoginFragment.kt
@@ -0,0 +1,52 @@
+package com.example.coalamobile.ui.login
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.coalamobile.R
+import com.firebase.ui.auth.AuthUI
+
+class LoginFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = LoginFragment()
+    }
+
+    private lateinit var viewModel: LoginViewModel
+    private var REQUEST_CODE = 1001
+    private val providers = arrayListOf(
+        AuthUI.IdpConfig.EmailBuilder().build(),
+        //AuthUI.IdpConfig.PhoneBuilder().build(),
+        AuthUI.IdpConfig.GoogleBuilder().build(),
+        //AuthUI.IdpConfig.FacebookBuilder().build(),
+        // AuthUI.IdpConfig.TwitterBuilder().build()
+                                            )
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        var view = inflater.inflate(R.layout.login_fragment, container, false)
+
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),
+            REQUEST_CODE
+        )
+
+        return view
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
