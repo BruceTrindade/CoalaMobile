2d51b9183a2de58a0d90c24f4d8fe50593af2713
Bruce Trindade <brucetrindade2000@gmail.com>
2021-06-22 00:10:27 -0300

    Coala Versão 1.03.12

--- app/src/main/java/com/example/coalamobile/HomeActivity.kt
+++ app/src/main/java/com/example/coalamobile/HomeActivity.kt
@@ -5,15 +5,20 @@ import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AlertDialog
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import com.example.coalamobile.ui.login.LoginFragment
+import com.firebase.ui.auth.AuthUI
 import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
 
 
 class HomeActivity : AppCompatActivity() {
 
     lateinit var toggle: ActionBarDrawerToggle
+    lateinit var auth: FirebaseAuth
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -21,14 +26,6 @@ class HomeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_home)
         setSupportActionBar(findViewById(R.id.toolbar))
         //Meu e-mail: brucetrindade2000@gmail.com
-        //Setando o fragment do login firebase
-        if (savedInstanceState == null){
-            supportFragmentManager
-                    .beginTransaction()
-                    .add(R.id.drawerLayout, LoginFragment())
-                    .commit()
-        }
-
         //setando a gaveta de menu
         val drawerLayout : DrawerLayout = findViewById(R.id.drawerLayout)
         val navView : NavigationView = findViewById(R.id.nav_view)
@@ -36,9 +33,23 @@ class HomeActivity : AppCompatActivity() {
         toggle = ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close)
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
-
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        navView.setNavigationItemSelectedListener {
+
+          navView.setNavigationItemSelectedListener {
+
+              if (it.itemId == R.id.Sair) {
+                  AlertDialog.Builder(this).apply {
+                      setTitle("Are you sure?")
+                      setPositiveButton("Yes") { _, _ ->
+
+                          FirebaseAuth.getInstance().signOut()
+
+
+                      }
+                      setNegativeButton("Cancel") { _, _ ->
+                      }
+                  }.create().show()
+              }
 
             when(it.itemId){
 
@@ -50,7 +61,7 @@ class HomeActivity : AppCompatActivity() {
                 R.id.MeusResultados -> Toast.makeText(applicationContext, "clicked Home", Toast.LENGTH_SHORT).show()
                 R.id.IndicarCoala -> Toast.makeText(applicationContext, "clicked Home", Toast.LENGTH_SHORT).show()
                 R.id.Ajuda -> Toast.makeText(applicationContext, "clicked Home", Toast.LENGTH_SHORT).show()
-                R.id.Sair -> Toast.makeText(applicationContext, "clicked Home", Toast.LENGTH_SHORT).show()
+                              /*R.id.Sair -> Toast.makeText(applicationContext, "clicked Home", Toast.LENGTH_SHORT).show()*/
             }
 
             true
